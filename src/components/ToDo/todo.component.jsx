54947d97a040eec438cd649406403afea76fe1f9
Amit T5 <amitt5@chetu.com>
2020-01-22 22:50:32 +0530

    add list on todo items

--- src/components/ToDo/todo.component.jsx
+++ src/components/ToDo/todo.component.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
 const ToDoComponent = (props) => {
+    const classes = useStyles();
     return (
-        <div>
-            <ul>
-                <li>{props.todo.id}</li>
-                <li>{props.todo.status}</li>
-                <li>{props.todo.title}</li>
-            </ul>
-        </div>
-    )
+        <Router>
+            <List className={classes.root}>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={props.todo.title} secondary={props.todo.id} />
+                </ListItem>
+            </List>
+        </Router>
+
+    );
 }
 export default ToDoComponent;
